a57abbe7f1aebfd6af5d22e6825078b2ac4f39e9
KJankowska <39523923+KJankowska@users.noreply.github.com>
2018-10-17 07:56:12 +0200

    Add files via upload
    clean. 
    add all files to folder TrackingApi

---
+++ TrackingAPI/src/main/java/com/tracking/TrackingAPI/event/domain/EventService.java
@@ -0,0 +1,33 @@
+package com.tracking.TrackingAPI.event.domain;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+/**
+ * Klasa serwisu, to ona wywołuje bezprośrednio metody repozytorium.
+ */
+@Service
+@Transactional
+public class EventService {
+
+    private EventRepository eventRepository;
+
+    public EventService(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
+
+    public Event addEvent(Event event) {
+        return eventRepository.save(event);
+    }
+
+    public List<Event> findLastEvents(int number) {
+        return eventRepository.findAll(
+                PageRequest.of(0, number,
+                        Sort.by("eventTime").descending()))
+                .getContent();
+    }
+}
